afea8de79b8cd9cbef1728c3ca6e0144842391f4
Luyue28 <zhan0067@hz.nl>
2025-04-13 00:01:41 +0200

    default user info displayed

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 import { getDatabase } from '../../database/initDB.js';
+import {
+    View,
+    Text,
+    Image,
+    ScrollView
+} from 'react-native';
+import { icons } from '../../constants/icons';
 
 type User = {
-    avatar?: string;
+    id: number;
     name?: string;
-    target_days?: number;
-    current_streak?: number;
+    // avatar?: string;
 };
 
 const Profile = () => {
@@ -23,15 +28,51 @@ const Profile = () => {
     }, []);
 
     return (
-        <View>
-            <Text>Profile</Text>
-            <Text>{user?.avatar}</Text>
-            <Text>Name: {user?.name || 'NoSmoke User'}</Text>
-            <Text>Target days: {user?.target_days}</Text>
-            <Text>Current streak: {user?.current_streak}</Text>
+    <ScrollView className="flex-1 bg-white">
+        {/* 用户信息卡片 */}
+        <View className="relative bg-white px-0 py-6">
+            {/* Edit Icon */}
+            <View className="absolute bottom-6 right-10">
+                <Image
+                    source={icons.edit}
+                    className="w-6 h-6"
+                    style={{ tintColor: '#6b7280' }}
+                />
+            </View>
+
+            <View className="items-center justify-center">
+                {/* 头像 */}
+                {/*暂时只使用这一张默认图片 使用require()直接读取 不经过数据库*/}
+                <Image
+                    source={require('../../assets/images/defaultAvatar.png')}
+                    className="w-24 h-24 rounded-full mb-2"
+                />
+
+                {/* 用户ID */}
+                {/*<Text className="text-2xl text-gray-800">*/}
+                {/*    {user?.id}*/}
+                {/*</Text>*/}
+
+                {/* 用户名 */}
+                <Text className="text-2xl text-gray-800">
+                    {user?.name || 'Unknown'}
+                </Text>
+            </View>
         </View>
+
+        {/* 分割线 */}
+        <View className="bg-white">
+            <View
+                style={{
+                    height: 1.5,
+                    backgroundColor: '#d1d5db',
+                    marginHorizontal: 20,
+                    marginBottom: 20,
+                }}
+            />
+        </View>
+    </ScrollView>
     )
 }
 
 export default Profile;
-const styles = StyleSheet.create({})
